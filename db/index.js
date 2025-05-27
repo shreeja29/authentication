539233428ebf8c364957e0f6fc49b20ff5648f5a
shreeja joshi <shreeja.joshi@rapidops.com>
2025-05-28 00:38:13 +0530

    update env

--- db/index.js
+++ db/index.js
@@ -1,14 +1,15 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.PGUSER,        // e.g. 'shreeja.joshi'
-  host: process.env.PGHOST,        // e.g. remote host like 'db.example.com'
-  database: process.env.PGDATABASE,// e.g. 'mydbi'
-  password: process.env.PGPASSWORD,// your DB password
-  port: process.env.PGPORT || 5432,
-  ssl: process.env.PGSSL === 'true' ? { rejectUnauthorized: false } : false,
+  user: "mydbi_user",        // e.g. 'shreeja.joshi'
+  host: "dpg-d0r0jgu3jp1c739c9ck0-a",        // e.g. remote host like 'db.example.com'
+  database: "mydbi",// e.g. 'mydbi'
+  password: "Fsqc6JeAv6GUE4DCo3Y9Xqj5QHwhpdms",// your DB password
+  port: 5432,
+  ssl: false,
 });
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
+
